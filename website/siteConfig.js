a8026cd92bded87aa7840f1e860e2633591efe2a
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2019-10-01 16:06:10 -0700

    chore: add readme (#7)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,18 +1,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const wallets = [
   {
     name: "XRP Tip Bot",
@@ -63,9 +51,6 @@ const siteConfig = {
     { href: 'https://forum.interledger.org', label: 'Forum' },
     { href: 'https://github.com/interledger/webmonetization.org', label: 'GitHub' },
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   providers,
   wallets,
