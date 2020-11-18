588e51a08197cea12c3af34b0ae8994bf00bd629
Sabine <sabine.bertram@mailbox.org>
2020-11-18 14:18:21 +0100

    fix: path

--- workers/exclusive-content/src/lib/receiptVerifier.ts
+++ workers/exclusive-content/src/lib/receiptVerifier.ts
@@ -1,6 +1,6 @@
 export async function verifyReceipt(verifier: string, receipt: string) {
   const endpoint = new URL(
-    verifier.endsWith('/') ? `${verifier}receipts` : `${verifier}/verify`,
+    verifier.endsWith('/') ? `${verifier}verify` : `${verifier}/verify`,
   )
   const response = await fetch(endpoint.href, { method: 'POST', body: receipt })
   return response.ok
