4e4191a23c5c6fd701592b7ecb1d1905d8704ef8
Sabine <sabine.bertram@mailbox.org>
2021-01-25 11:40:28 +0100

    feat: import static file to render as independent script

--- src/page-support/exclusive-content/components/independent-script.js
+++ src/page-support/exclusive-content/components/independent-script.js
@@ -1,148 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { CopyContent } from './copy-content'
 
-const WORKER = 'https://webmonetization.org/api/exclusive-content'
-
 export function IndependentScript() {
-  const independentScript = `<script>
-  const { data, paymentPointer } = gatherExclusive();
+  const [script, setScript] = useState('')
 
-  if (document.monetization !== undefined) {
-    setMeta(paymentPointer);
+  useEffect(() => {
+    const getJS = () =>
+      fetch('../../../../static/js/exclusive-content.js').then((r) => r.text())
 
-    document.monetization.addEventListener("monetizationpending", () => {
-      if (data.length) {
-        document.querySelectorAll(".exclusiveMessage").forEach((element) => {
-          element.innerHTML =
-            "<p>⸻ ⏰ Waiting for Web Monetization to start to unlock this content. ⸻</p>";
-        });
-      }
-    });
-
-    document.monetization.addEventListener("monetizationstart", () => {
-      if (data.length) {
-        document.querySelectorAll(".exclusiveMessage").forEach((element) => {
-          element.innerHTML =
-            "<p>⸻ ⏰ Waiting for Web Monetization receipt verification to unlock this content. ⸻</p>";
-        });
-      }
-    });
-
-    document.monetization.addEventListener("monetizationprogress", (event) => {
-      if (data.length) {
-        unlockExclusiveContent(data.pop(), event.detail.receipt);
-      }
-    });
-  } else {
-    document.querySelectorAll(".exclusiveMessage").forEach((element) => {
-      element.innerHTML =
-        "<p>⸻ 🔒 This content is exclusive for users with Web Monetization enabled. ⸻</p>";
-    });
-  }
-
-  function gatherExclusive() {
-    const data = [];
-    const pointers = [];
-    document.querySelectorAll(".exclusive").forEach((element) => {
-      data.push({
-        id: element.getAttribute("id"),
-        paymentPointer: element.getAttribute("paymentpointer"),
-        cypherText: element.getAttribute("cyphertext"),
-        cypherVerifier: element.getAttribute("cypherverifier"),
-        initVector: element.getAttribute("initvector"),
-        nonce: element.getAttribute("nonce"),
-      });
-      pointers.push(element.getAttribute("proxypaymentpointer"));
-    });
-    const paymentPointer = pointers[Math.floor(Math.random() * pointers.length)];
-    return { data, paymentPointer };
-  }
-
-  function setMeta(proxyPaymentPointer) {
-    const meta = document.createElement("meta");
-    meta.name = "monetization";
-    meta.content = proxyPaymentPointer;
-    document.getElementsByTagName("head")[0].appendChild(meta);
-  }
-
-  function unlockExclusiveContent(data, receipt) {
-    fetch("${WORKER}/deriveKey", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        paymentPointer: data.paymentPointer,
-        nonce: data.nonce,
-        encVerifier: data.cypherVerifier,
-        initVector: data.initVector,
-        receipt: receipt,
-      }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        decryptContent(responseData.key, data.cypherText, data.initVector).then(
-          (plaintext) => {
-            const element = document.querySelector(\`#\${data.id}\`);
-            element.querySelector(".exclusiveMessage").innerHTML =
-              "<p>⸻ 🔓 Enjoy your exclusive content! ⸻</p>";
-            element.querySelector(".exclusiveContent").innerHTML = plaintext;
-          }
-        );
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-  }
-
-  async function decryptContent(keyString, cypherText, initVector) {
-    const key = await importKey(str2ab(keyString));
-    const plaintext = await decrypt(str2ab(cypherText), key, encode(initVector));
-    return decode(plaintext);
-  }
-
-  async function decrypt(cypherText, key, initVector) {
-    return window.crypto.subtle.decrypt(
-      {
-        name: "AES-GCM",
-        iv: initVector,
-      },
-      key,
-      cypherText
-    );
-  }
-
-  async function importKey(keyBuffer) {
-    return window.crypto.subtle.importKey("raw", keyBuffer, "AES-GCM", false, [
-      "encrypt",
-      "decrypt",
-    ]);
-  }
-
-  function encode(str) {
-    const encoder = new TextEncoder("utf-8");
-    return encoder.encode(str);
-  }
-
-  function decode(buf) {
-    const decoder = new TextDecoder("utf-8");
-    return decoder.decode(buf);
-  }
-
-  function str2ab(str) {
-    const decodedString = window.atob(str);
-    const ab = new ArrayBuffer(decodedString.length);
-    const abView = new Uint8Array(ab);
-    for (let i = 0, strLen = decodedString.length; i < strLen; i++) {
-      abView[i] = decodedString.charCodeAt(i);
-    }
-    return ab;
-  }
+    getJS().then(
+      (r) => {
+        const independentScript = `<script>
+${r.replace('export ', '')}
+exclusiveContent()
 </script>`
+        setScript(independentScript)
+      },
+      (e) => {
+        setScript(e)
+      }
+    )
+  })
 
   return (
     <>
@@ -156,7 +36,7 @@ export function IndependentScript() {
           language='htmlbars'
           style={docco}
         >
-          {independentScript}
+          {script}
         </SyntaxHighlighter>
       </div>
     </>
