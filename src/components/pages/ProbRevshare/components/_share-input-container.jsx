1b0dcdeef5b99d8a4bb27425f47000ee9f8dac3b
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-08-01 18:52:55 +0800

    Fix table styling

--- src/components/pages/ProbRevshare/components/_share-input-container.jsx
+++ src/components/pages/ProbRevshare/components/_share-input-container.jsx
@@ -2,17 +2,19 @@ import styles from '../styles.module.css'
 
 export function ShareInputContainer({ children }) {
   return (
-    <table className={styles.revShareTable}>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Pointer</th>
-          <th>Weight</th>
-          <th>Percent</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>{children}</tbody>
-    </table>
+    <div class={styles.tableWrapper}>
+      <table className={styles.revShareTable}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Pointer</th>
+            <th>Weight</th>
+            <th>Percent</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{children}</tbody>
+      </table>
+    </div>
   )
 }
