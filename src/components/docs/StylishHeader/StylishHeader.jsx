37d33ec318d0f208989bf979b09c10ba3e6fcbaf
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-08-17 11:53:50 +0800

    More tweaks based on content team feedback

--- src/components/docs/StylishHeader/StylishHeader.jsx
+++ src/components/docs/StylishHeader/StylishHeader.jsx
@@ -1,5 +1,5 @@
 import styles from './StylishHeader.module.css'
 
-export default function StylishHeader({ children, href, withIcon = true }) {
-  return <p className={styles.highlight}>{children}</p>
+export default function StylishHeader({ children }) {
+  return <p className={`${styles.highlight} heading`}>{children}</p>
 }
