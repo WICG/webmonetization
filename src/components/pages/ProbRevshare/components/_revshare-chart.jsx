ec78d73557b51a1c0ff25447ffc78497bc625269
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-08-18 20:31:14 +0800

    Fix firefox chart size

--- src/components/pages/ProbRevshare/components/_revshare-chart.jsx
+++ src/components/pages/ProbRevshare/components/_revshare-chart.jsx
@@ -16,20 +16,18 @@ export function RevshareChart() {
   }
 
   return (
-    <div className={styles.chartWrapper}>
-      <PieChart
-        className={styles.revShareChart}
-        style={{ width: '100%', height: '250' }}
-        data={chartData}
-        label={genLabel}
-        radius={40}
-        labelStyle={() => ({
-          fontFamily:
-            'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-          fontSize: '6px',
-        })}
-        labelPosition={112}
-      />
-    </div>
+    <PieChart
+      className={styles.revShareChart}
+      style={{ height: '250px', width: '100%' }}
+      data={chartData}
+      label={genLabel}
+      radius={40}
+      labelStyle={() => ({
+        fontFamily:
+          'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        fontSize: '6px',
+      })}
+      labelPosition={112}
+    />
   )
 }
