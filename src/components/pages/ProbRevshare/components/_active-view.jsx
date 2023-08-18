dfccc53963fa2baa77da7bf41fcbfdbc37dfe0f2
Melissa Henderson <57110301+melissahenderson@users.noreply.github.com>
2023-08-18 11:50:13 -0400

    revshare cleanup, etc

--- src/components/pages/ProbRevshare/components/_active-view.jsx
+++ src/components/pages/ProbRevshare/components/_active-view.jsx
@@ -23,12 +23,12 @@ export function ActiveView() {
   } else if (view === ViewStates.Import) {
     return (
       <>
-        <h2 className='heading3'>Import an existing revshare</h2>
+        <h2 className='heading3'>Import existing revshare configuration</h2>
         <p className={styles.text}>
-          Click <b>Import Existing Revshare</b> to edit a revshare. Then, enter your current <code>link</code> element into the field and click <b>Import</b>. The table will populate with your existing revshare's details.
+          Enter your current monetization <code>link</code> element into the field and click <b>Import</b>. The table will populate with your existing revshare's configuration.
         </p>
         <p className={styles.text}>
-            If you make any changes, the <code>link</code> generated on this page will change. Be sure to replace your old <code>link</code> element with the updated version. Check out <a href="/docs/guides/monetize-page">Add Web Monetization to a page</a> if you need help.
+            If you make any changes to the table, your monetization <code>link</code> will change. Be sure to replace your old <code>link</code> element with the updated version. Check out <a href="/docs/guides/monetize-page" target="_blank">Add Web Monetization to a page</a> if you need help.
         </p>
         <ImportView />
       </>
