a20a471c6ce420b085c722e85e67664b8ea61962
Nicholas Dudfield <ndudfield@gmail.com>
2023-09-29 20:39:36 +0700

    feat: add respec build script (#389)
    * feat: add respec build script
    
    * feat: update formerEditors

---
+++ src/pages/specification/includes/verify-payment-example.js
@@ -0,0 +1,25 @@
+/** @type {MonetizationEvent} event */
+async function verifyPayment(event) {
+  // Legacy receivers don't support returning incoming payment URLs
+  if (!event.incomingPayment) {
+    throw new Error('No incoming payment URL')
+  }
+
+  // Poll the incoming payment to check the amount received
+  const response = await fetch(event.incomingPayment, {
+    credentials: 'same-origin',
+    mode: 'same-origin',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  if (response.ok) {
+    // The incoming payment was fetched successfully
+    const { receivedAmount } = JSON.parse(await response.json())
+    const { amount, assetCode, assetScale } = receivedAmount
+    console.log(`Received ${assetCode}${amount / 10 ** assetScale}.`)
+    return receivedAmount
+  }
+}
