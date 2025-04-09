6eff3f76f65d54c7ac130540d601cf9114c599a0
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2025-04-09 20:46:52 +0800

    chore: bump dependencies to latest versions and adjust i18n implementation on pages (#518)

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const showDefaultLang = false
+export const languages = {
+  en: 'English',
+  es: 'Español',
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'site.title': 'Web Monetization',
+    'site.description':
+      'The Web Monetization API allows websites to automatically and passively receive payments from Web Monetization-enabled visitors.',
+    'nav.documentation': 'Documentation',
+    'nav.specification': 'Specification',
+  },
+  es: {
+    'site.title': 'Web Monetization',
+    'site.description':
+      'The Web Monetization API allows websites to automatically and passively receive payments from Web Monetization-enabled visitors.',
+    'nav.documentation': 'Documentación',
+    'nav.specification': 'Especificación',
+  },
+} as const
