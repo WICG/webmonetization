0a9a860dafda2bec3958e3a831982dcffb478b4a
Anca Matei <ancamatei2012@gmail.com>
2025-10-08 16:32:16 +0300

    undo formatting changes

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -4,67 +4,63 @@ import publisherIconActive from '@assets/dropdown-publisher-active.png?inline'
 import supporterIconInactive from '@assets/dropdown-supporter-inactive.png?inline'
 import supporterIconActive from '@assets/dropdown-supporter-active.png?inline'
 
-export const TOOL_LABEL_KEYS = [
-  'nav.pub-tools',
-  'nav.supp-tools',
-  'nav.dev-tools',
-] as const
-export type ToolLabelKey = (typeof TOOL_LABEL_KEYS)[number]
+export const TOOL_LABEL_KEYS = ['nav.pub-tools', 'nav.supp-tools', 'nav.dev-tools'] as const;
+export type ToolLabelKey = typeof TOOL_LABEL_KEYS[number];
 
 interface ToolsSubmenuItem {
-  imageInactive: {
-    src: string
-    width: number
-    height: number
-  }
-  imageActive: {
-    src: string
-  }
-  translationKey: ToolLabelKey
-  path: string
-  event: string
+    imageInactive: {
+        src: string,
+        width: number,
+        height: number
+    },
+    imageActive: {
+        src: string
+    },
+    translationKey: ToolLabelKey,
+    path: string,
+    event: string
 }
 
 export type ToolsSubmenu = Array<ToolsSubmenuItem>
 
 export const toolsSubmenu: ToolsSubmenu = [
-  {
-    imageInactive: {
-      src: publisherIconInactive,
-      width: 460,
-      height: 460,
+    {
+        imageInactive: {
+            src: publisherIconInactive,
+            width: 460,
+            height: 460
+        },
+        imageActive: {
+            src: publisherIconActive
+        },
+        translationKey: 'nav.pub-tools',
+        path: '/publishers',
+        event: 'Nav link - Publishers'
     },
-    imageActive: {
-      src: publisherIconActive,
+    {
+        imageInactive: {
+            src: supporterIconInactive,
+            width: 460,
+            height: 460
+        },
+        imageActive: {
+            src: supporterIconActive
+        },
+        translationKey: 'nav.supp-tools',
+        path: '/supporters',
+        event: 'Nav link - Supporters'
     },
-    translationKey: 'nav.pub-tools',
-    path: '/publishers',
-    event: 'Nav link - Publishers',
-  },
-  {
-    imageInactive: {
-      src: supporterIconInactive,
-      width: 460,
-      height: 460,
-    },
-    imageActive: {
-      src: supporterIconActive,
-    },
-    translationKey: 'nav.supp-tools',
-    path: '/supporters',
-    event: 'Nav link - Supporters',
-  },
-  {
-    imageInactive: {
-      src: developerIcon,
-      width: 460,
-      height: 460,
-    },
-    imageActive: {
-      src: developerIcon,
-    },
-    translationKey: 'nav.dev-tools',
-    path: '/developers',
-    event: 'Nav link - Developers',
-  },
+    {
+        imageInactive: {
+            src: developerIcon,
+            width: 460,
+            height: 460
+        },
+        imageActive: {
+            src: developerIcon
+        },
+        translationKey: 'nav.dev-tools',
+        path: '/developers',
+        event: 'Nav link - Developers'
+    }
 ]
