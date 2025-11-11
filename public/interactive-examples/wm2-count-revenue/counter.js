dcefdb92a7640810a87c16515a3d0443e7c01826
Thomas Steiner <tomac@google.com>
2025-11-11 05:20:42 +0100

    fix(examples/counter): use the `Intl.NumberFormat` API (#608)

--- public/interactive-examples/wm2-count-revenue/counter.js
+++ public/interactive-examples/wm2-count-revenue/counter.js
@@ -2,19 +2,19 @@ let total = 0;
 
 if (window.MonetizationEvent) {
   const link = document.querySelector('link[rel="monetization"]');
+  const contribution = document.getElementById('contribution');
 
-  link.addEventListener("monetization", (event) => {
+  link.addEventListener('monetization', (event) => {
     const {
       amountSent: { value, currency },
     } = event;
     console.log(`Browser sent ${currency} ${value}.`);
 
-    if (total === 0) {
-      document.getElementById("currency").innerText = currency;
-    }
-
     total += Number(value);
 
-    document.getElementById("total").innerText = total.toFixed(9);
+    contribution.innerText = new Intl.NumberFormat(navigator.language, {
+      style: 'currency',
+      currency,
+    }).format(total);
   });
 }
