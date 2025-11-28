050fe9df9e7a6f700a40d010cbdd9b00e4d47a3d
Jonathan Matthey <mattheyj@gmail.com>
2025-11-28 19:40:55 +0000

    add eslint / prettier config

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginAstro from 'eslint-plugin-astro'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  globalIgnores(['dist', '.astro', 'node_modules', 'public', '**/*.min.js']),
+  {
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'astro/no-set-text-directive': 'error'
+    }
+  },
+  eslintConfigPrettier
+])
+
+
