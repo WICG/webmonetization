397f14dcc5bd944cec63c934828ce909bb828696
Melissa Henderson <57110301+melissahenderson@users.noreply.github.com>
2025-12-19 10:08:09 -0500

    docs: break out publisher tools docs into individual pages (#623)
    * docs: fixes #620
    
    Split publisher tools page into multiple pages, one per tool
    
    * docs: updated based on feedback
    
    Still considering feedback on a preview for the widget tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -100,6 +100,28 @@ export default defineConfig({
               label: 'WordPress plugin',
               link: '/publishers/wordpress',
             },
+            {
+              label: 'Publisher tools',
+              collapsed: true,
+              items: [
+                {
+                  label: 'Banner tool',
+                  link: '/publishers/banner-tool'
+                },
+                {
+                  label: 'Widget tool',
+                  link: '/publishers/widget-tool'
+                },
+                {
+                  label: 'Link tag generator',
+                  link: '/publishers/link-tag-tool'
+                },
+                {
+                  label: 'Probabalistic revshare generator',
+                  link: '/publishers/revshare-tool'
+                }, 
+              ],
+            },
           ],
         },
         {
@@ -213,10 +235,6 @@ export default defineConfig({
               link: '/developers/libraries',
             },
             {
-              label: 'Publisher tools',
-              link: '/developers/tools',
-            },
-            {
               label: 'Web Monetization Specification',
               link: 'https://webmonetization.org/specification/',
               attrs: {
@@ -264,6 +282,7 @@ export default defineConfig({
     '/docs/explainer': '/docs',
     '/docs/intro/sending-payments': '/supporters/about-sending',
     '/developers/about-receiving': '/publishers/about-receiving',
+    '/developers/tools': '/publishers/banner-tool',
     '/docs/intro/receiving-payments': '/publishers/about-receiving',
     '/docs/intro/web-monetization-flow': '/docs',
     '/docs/intro/web-monetization-extension': '/supporters/get-started',
